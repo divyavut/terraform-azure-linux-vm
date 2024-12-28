772cfe3945ede51237a0e2d8305b3bba66295551
divyavut <vdivyaaws@gmail.com>
2024-12-28 15:54:23 -0500

    Azure Linux VM

--- main.tf
+++ main.tf
@@ -1,55 +1,54 @@
-resource "azurerm_resource_group" "myrg" {
-   name     = var.rg_name
-   location = var.rg_location
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
 }
 
-resource "azurerm_virtual_network" "myvnet" {
-   name                 = var.vnet_name
-   resource_group_name  = azurerm_resource_group.myrg.name
-   location             = azurerm_resource_group.myrg.location
-   address_space        = var.address_space
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "subnet" {
-    name                    = var.subnet_name
-    resource_group_name     = azurerm_resource_group.myrg.name
-    virtual_network_name    = azurerm_virtual_network.myvnet.name
-    address_prefixes        = var.address_prefixes
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "mypip" {
-  name                = var.pip
-  location            = azurerm_resource_group.myrg.location
-  resource_group_name = azurerm_resource_group.myrg.name
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   allocation_method   = "Static"
 }
 
-resource "azurerm_network_interface" "mynic" {
-  name                = "my-nic-1"
-  location            = azurerm_resource_group.myrg.location
-  resource_group_name = azurerm_resource_group.myrg.name
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "my-ip-config"
-    subnet_id                     = azurerm_subnet.subnet.id
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.mypip.id
+    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "myvm" {
-  name                  = "Vm27"
-  location              = azurerm_resource_group.myrg.location
-  resource_group_name   = azurerm_resource_group.myrg.name
+resource "azurerm_linux_virtual_machine" "example" {
+  name                  = "example-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
   size                  = "Standard_B1s"
-  admin_username        = "adminuser"
+  admin_username        = "azureuser"
 
-  network_interface_ids = [azurerm_network_interface.mynic.id]
+  network_interface_ids = [azurerm_network_interface.example.id]
 
   admin_ssh_key {
-    username   = "adminuser"
-    # ssh-keygen -t rsa -f C:\Terraform\SSHKeys\id_rsa  <-- command to generate keys in windows
-    public_key = file("C:/Terraform/T20/Terraform/SSH_Keys/id_rsa.pub") 
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
   }
 
   os_disk {
@@ -57,10 +56,32 @@ resource "azurerm_linux_virtual_machine" "myvm" {
     storage_account_type = "Standard_LRS"
   }
 
-    source_image_reference {
+  source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
+}
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
 }
